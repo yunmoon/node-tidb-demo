9a82cdfe32d474f3f88b0530c08be138a12c2b4a
yunmoon <meiyun@zhiyuanhui.com>
2019-09-12 16:47:39 +0800

    update

--- src/entity/inoutrecord.ts
+++ src/entity/inoutrecord.ts
@@ -1,5 +1,5 @@
 
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "zonetk-core";
+import { Entity, Column, PrimaryColumn, CreateDateColumn, UpdateDateColumn } from "zonetk-core";
 @Entity({ name: "inoutrecords1" })
 export class Inoutrecord {
 
@@ -18,7 +18,7 @@ export class Inoutrecord {
     @Column()
     userCardNo: string;
 
-    @PrimaryGeneratedColumn()
+    @PrimaryColumn()
     deviceOrderNo: string;
 
     @Column()
